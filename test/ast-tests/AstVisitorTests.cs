71bec3ac149dd47951afb3ac6073b9005376c056
Andrew Matthews <A30006806@agl.com.au>
2024-10-05 15:02:07 +1000

    validating immutable AST elements

---
+++ test/ast-tests/AstVisitorTests.cs
@@ -0,0 +1,69 @@
+﻿using ast;
+using ast_generated;
+using FluentAssertions;
+
+namespace ast_tests;
+public class AstVisitorTests
+{
+    [Fact]
+    public void can_visit_and_recreate_an_AST_while_preserving_its_other_data()
+    {
+        var def = CreateAssemblyDef();
+        var expectedAnnotation = Guid.NewGuid().ToString();
+        def.Annotations["someKey"] = expectedAnnotation;
+        var sut = new DefaultRecursiveDescentVisitor();
+        var def2 = (AssemblyDef)sut.Visit(def);
+        def2.Should().NotBeNull();
+        def2.Should().BeEquivalentTo(def);
+        def2.Annotations["someKey"].Should().Be(expectedAnnotation);
+    }
+
+    AssemblyDef CreateAssemblyDef()
+    {
+        return new AssemblyDef()
+        {
+            SourceContext = new("", 1, 1, "blah"),
+            Annotations = [],
+            AssemblyRefs = [],
+            ClassDefs = [createClassDef("MyType1", 1),createClassDef("MyType2", 2)],
+            Name = AssemblyName.From("MyAsm"),
+            Type = CreateTypeMetadata("myAsm", 0, SymbolKind.Assembly),
+            Parent = null,
+            PublicKeyToken = "ajhsgjsfdhg",
+            Version = "0.1.1.1",
+            Visibility = Visibility.Public
+        };
+    }
+
+    private static TypeMetadata CreateTypeMetadata(string name, ushort typeId, SymbolKind symbolKind = SymbolKind.Assembly)
+    {
+        return new()
+        {
+            TypeId = TypeId.From(typeId),
+            Symbol = new Symbol()
+            {
+                Name = name,
+                Kind = symbolKind,
+                Location =
+                    new()
+                    {
+                        Column = 1, Filename = "ashksah", Line = 1, OriginalText = "kjahgsfdgasfjd"
+                    }
+            }
+        };
+    }
+
+    private static ClassDef createClassDef(string name, ushort typeId)
+    {
+        return new ClassDef()
+        {
+            SourceContext = new("", 1, 1, "blah"),
+            Annotations = [],
+            MemberDefs = [],
+            Namespace = "std",
+            Parent = null,
+            Type = CreateTypeMetadata(name, typeId, SymbolKind.ClassDef),
+            Visibility = Visibility.Public
+        };
+    }
+}
