8deeef0ea0af60b5ab1a50d7b74b218706ff9a75
Andrew Matthews <A30006806@agl.com.au>
2025-03-24 16:46:07 +1100

    Enhance tests for function parameter destructuring
    Added assertions to validate the properties of the first function in the first module of the parsed AST when it is an `AssemblyDef`. Ensured the function has one parameter named "p" with a destructure definition containing two valid property bindings.

--- test/ast-tests/DestructuringVisitorTests.cs
+++ test/ast-tests/DestructuringVisitorTests.cs
@@ -12,5 +12,16 @@ public class DestructuringVisitorTests : VisitorTestsBase
         var ast = (AstThing)ParseProgram("recursive-destructuring.5th");
         ast = new DestructuringVisitor().Visit(ast);
         ast.Should().NotBeNull();
+        if (ast is AssemblyDef asm)
+        {
+            var fn_calculate_bmi = asm.Modules[0].Functions[0];
+            fn_calculate_bmi.Should().NotBeNull();
+            fn_calculate_bmi.Params.Should().HaveCount(1);
+            fn_calculate_bmi.Params[0].Name.Should().Be("p");
+            fn_calculate_bmi.Params[0].DestructureDef.Should().NotBeNull();
+            fn_calculate_bmi.Params[0].DestructureDef.Bindings.Should().HaveCount(2);
+            fn_calculate_bmi.Params[0].DestructureDef.Bindings[0].ReferencedProperty.Should().NotBeNull();
+            fn_calculate_bmi.Params[0].DestructureDef.Bindings[1].ReferencedProperty.Should().NotBeNull();
+        }
     }
 }
