272f1a713665384174bfc73aa51d8b39c30239c4
Andrew Matthews <matthews.andrew@gmail.com>
2025-08-20 20:14:34 +1000

    Refactor Equals and GetHashCode methods in FunctionSignature; streamline guarded destructuring test

--- src/ast-model/TypeSystem/FunctionSignature.cs
+++ src/ast-model/TypeSystem/FunctionSignature.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace ast_model.TypeSystem;
 
 public record FunctionSignature : IFunctionSignature
@@ -7,4 +9,28 @@ public record FunctionSignature : IFunctionSignature
     public MemberName Name { get; set; }
     public FifthType ReturnType { get; set; }
     public FifthType? DeclaringType { get; set; }
+
+    public virtual bool Equals(FunctionSignature? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+
+        var thisParams = FormalParameterTypes ?? Array.Empty<FifthType>();
+        var otherParams = other.FormalParameterTypes ?? Array.Empty<FifthType>();
+
+        return EqualityComparer<MemberName>.Default.Equals(Name, other.Name)
+            && EqualityComparer<FifthType>.Default.Equals(ReturnType, other.ReturnType)
+            && thisParams.SequenceEqual(otherParams);
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(Name);
+        hash.Add(ReturnType);
+        var ps = FormalParameterTypes ?? Array.Empty<FifthType>();
+        foreach (var p in ps)
+            hash.Add(p);
+        return hash.ToHashCode();
+    }
 }
