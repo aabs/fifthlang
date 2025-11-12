2dea6063f47ba802212268af6de2e77daf833b86
Andrew Matthews <matthews.andrew@gmail.com>
2025-11-12 14:52:06 +1100

    chore: commit pending changes before .NET 10 upgrade

--- src/parser/grammar/grammar/FifthParser.cs
+++ src/parser/grammar/grammar/FifthParser.cs
@@ -3861,7 +3861,7 @@ public partial class FifthParser : FifthParserBase {
 			{
 			State = 469;
 			if (!( InputStream.LA(1) == LESS && InputStream.LA(2) == IDENTIFIER && (InputStream.LA(3) == COMMA || InputStream.LA(3) == COLON)
-					)) throw new FailedPredicateException(this, " InputStream.LA(1) == LESS && InputStream.LA(2) == IDENTIFIER && (InputStream.LA(3) == COMMA || InputStream.LA(3) == COLON)\n\t\t");
+					)) throw new FailedPredicateException(this, " InputStream.LA(1) == LESS && InputStream.LA(2) == IDENTIFIER && (InputStream.LA(3) == COMMA || InputStream.LA(3) == COLON)\r\n\t\t");
 			State = 472;
 			ErrorHandler.Sync(this);
 			switch ( Interpreter.AdaptivePredict(TokenStream,39,Context) ) {
